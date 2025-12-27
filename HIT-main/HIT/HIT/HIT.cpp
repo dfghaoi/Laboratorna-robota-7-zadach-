23e588f83fb4bef15413bb47cd5889c5bbea3b65
dfghaoi <85017723+dfghaoi@users.noreply.github.com>
2025-12-27 23:41:17 +0100

    Add files via upload

---
+++ HIT-main/HIT/HIT/HIT.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int x0, y0, x1, y1, R;
+	srand(time(0));
+	x0 = rand() % 10;
+	y0 = rand() % 10;
+	R = rand() % 5 + 1;
+	//cout << "[DEBUG] Центр круга: (x = " << x0 << ", y = " << y0 << ", R = " << R <<")." << endl;
+	cout << "Ваш Х = "; cin >> x1;
+	cout << "Ваш Y = "; cin >> y1;
+	if ((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0) <= R * R)
+		cout << "Точка " << x0 << ", " << y0 << " потрапляе в коло з центром в точці " << x1 << ", " << y1 << " радіусом " << R << "." << endl;
+	else
+		cout << "Точка " << x0 << ", " << y0 << " не потрапляе в коло з центром в точці " << x1 << ", " << y1 << " радіусом " << R << "." << endl;
+}
