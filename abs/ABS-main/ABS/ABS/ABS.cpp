e6049728414479d0e5639df77bcdccb1b5f08ef0
dfghaoi <85017723+dfghaoi@users.noreply.github.com>
2025-12-29 12:55:26 +0100

    Add files via upload

---
+++ abs/ABS-main/ABS/ABS/ABS.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+	cout << "X = "; cin >> x;
+	if (x < 0) cout << "|" << x << "| = " << -x << endl;
+	else cout << "|" << x << "| = " << x << endl;
+}
